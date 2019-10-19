fbb20b076058c71b99a5df9efa556319c683d237
Shubham Roy <shubham.roy@cognizant.com>
2019-10-20 00:57:38 +0530

    Modified path

--- ilp_react/ecosystem.config.js
+++ ilp_react/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
       name: "react_app",
-      script: "start",
+      script: "./node_modules/react-scripts/scripts/start",
       env: {
         NODE_ENV: "development",
       },
