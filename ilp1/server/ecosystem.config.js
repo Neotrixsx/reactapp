b6b117ae183cfc1cb9c5617c0116d375b70799c8
Shubham Roy <shubham.roy@cognizant.com>
2019-10-20 01:07:54 +0530

    Modified path

--- ilp1/server/ecosystem.config.js
+++ ilp1/server/ecosystem.config.js
@@ -1,12 +1,7 @@
 module.exports = {
     apps : [{
       name: "server_app",
-      script: "start",
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      }
+      script: "npm",
+      args:"start",
     }]
   }
